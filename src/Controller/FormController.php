4b38a412a9bad68dfc616feb4b55a814885b3ee4
Cesar Giraldo <cesargiraldo1108@gmail.com>
2026-06-02 20:36:42 -0500

    feat: added new design example with tailwind

---
+++ src/Controller/FormController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FormController extends AbstractController
+{
+    #[Route('/form', name: 'app_form')]
+    public function index(): Response
+    {
+        return $this->render('form/index.html.twig', [
+            'controller_name' => 'FormController',
+        ]);
+    }
+}
